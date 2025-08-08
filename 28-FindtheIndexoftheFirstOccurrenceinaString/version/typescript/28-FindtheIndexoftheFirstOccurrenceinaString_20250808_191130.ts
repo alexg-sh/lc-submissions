1c80e03d883643f8b0b5b731e72a299b26bf0013
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-08 19:11:31 +0100

    Create 28-FindtheIndexoftheFirstOccurrenceinaString

---
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/version/typescript/28-FindtheIndexoftheFirstOccurrenceinaString_20250808_191130.ts
@@ -0,0 +1,17 @@
+// Last updated: 8/8/2025, 7:11:30 PM
+function strStr(haystack: string, needle: string): number {
+    let m = needle.length;
+    let n = haystack.length;
+
+    for (let windowStart = 0; windowStart <= n - m; windowStart++) {
+        for (let i = 0; i < m; i++) {
+            if (needle[i] != haystack[windowStart + i]) {
+                break;
+            }
+            if (i == m - 1) {
+                return windowStart;
+            }
+        }
+    }
+    return -1;
+};
