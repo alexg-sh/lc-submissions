63a28fed5f84da3634ace19fb317b9404c6b3d14
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-10 21:19:18 +0100

    File updated at 8/10/2025, 9:19:18 PM

--- 1492-ThekthFactorofn/1492-ThekthFactorofn.ts
+++ 1492-ThekthFactorofn/1492-ThekthFactorofn.ts
@@ -1,4 +1,4 @@
-// Last updated: 8/10/2025, 9:18:59 PM
+// Last updated: 8/10/2025, 9:19:18 PM
 function kthFactor(n: number, k: number): number {
     let idx: number = 0;
     for (let i = 1; i <= n; i++) {
