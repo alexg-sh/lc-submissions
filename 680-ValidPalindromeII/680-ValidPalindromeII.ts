62eb4af16ebb0f4bc21c821786f4ab0d63d703b0
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-13 20:33:48 +0100

    Create 680-ValidPalindromeII

---
+++ 680-ValidPalindromeII/680-ValidPalindromeII.ts
@@ -0,0 +1,25 @@
+// Last updated: 8/13/2025, 8:33:48 PM
+// TypeScript
+
+function validPalindrome(s: string): boolean {
+    let l = 0;
+    let r = s.length - 1;
+    
+    while (l < r) {
+        if (s[l] !== s[r]) {
+            return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
+        }
+        l++;
+        r--;
+    }
+    return true;
+}
+
+function isPalindrome(s: string, l: number, r: number): boolean {
+    while (l < r) {
+        if (s[l] !== s[r]) return false;
+        l++;
+        r--;
+    }
+    return true;
+}
