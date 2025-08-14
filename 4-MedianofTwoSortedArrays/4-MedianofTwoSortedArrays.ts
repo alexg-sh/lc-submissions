962f80f70dce509ad3f0f6d3deb7dbea980c0c0d
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-14 21:07:51 +0100

    Create 4-MedianofTwoSortedArrays

---
+++ 4-MedianofTwoSortedArrays/4-MedianofTwoSortedArrays.ts
@@ -0,0 +1,11 @@
+// Last updated: 8/14/2025, 9:07:50 PM
+function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
+    const array = [...nums1, ...nums2].sort((a, b) => a - b);
+    const mid = Math.floor(array.length / 2);
+
+    if (array.length % 2 === 0) {
+        return (array[mid] + array[mid - 1]) / 2;
+    } else {
+        return array[mid];
+    }
+}
