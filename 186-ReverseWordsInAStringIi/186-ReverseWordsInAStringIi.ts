b1156549ff0b569acbc1ce0081f9abc713371272
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-16 22:30:36 +0100

    Create 186-ReverseWordsInAStringIi

---
+++ 186-ReverseWordsInAStringIi/186-ReverseWordsInAStringIi.ts
@@ -0,0 +1,19 @@
+// Last updated: 8/16/2025, 10:30:35 PM
+function reverseWords(s: string[]): void {
+    s.reverse();
+    let l= 0,r= 0;
+    while (l<s.length){
+        while (r<s.length && s[r]!=' '){
+            r++;
+        }
+        let t=r-1;
+        while(l<t){
+            const temp = s[l];
+            s[l] = s[t]
+            s[t]= temp
+            l++, t--;
+        }
+        r++
+        l=r
+    }    
+};
