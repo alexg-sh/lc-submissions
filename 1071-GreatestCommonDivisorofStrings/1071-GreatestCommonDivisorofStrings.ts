092070a58320a0cf174eab526ddd224c9a25b77d
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-16 12:00:29 +0100

    Create 1071-GreatestCommonDivisorofStrings

---
+++ 1071-GreatestCommonDivisorofStrings/1071-GreatestCommonDivisorofStrings.ts
@@ -0,0 +1,15 @@
+// Last updated: 8/16/2025, 12:00:29 PM
+function gcdOfStrings(str1: string, str2: string): string {
+    if (str1 + str2 !== str2 + str1) {
+        return "";
+    }
+    const maxLength = myFunction(str1.length, str2.length);
+    return str1.substring(0, maxLength);
+};
+
+function myFunction(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    return myFunction(b, a % b);
+}
