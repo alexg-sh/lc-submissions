65323dc8351766407c2d92e28f622365c35de37d
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-17 22:27:59 +0100

    Create 3110-ScoreofaString

---
+++ 3110-ScoreofaString/3110-ScoreofaString.ts
@@ -0,0 +1,8 @@
+// Last updated: 8/17/2025, 10:27:59 PM
+function scoreOfString(s: string): number {
+    let totalScore = 0;
+    for (let i = 0; i < s.length - 1; i++) {
+        totalScore += Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1));
+    }
+    return totalScore;
+}
