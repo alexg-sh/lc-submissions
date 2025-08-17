c12fce0ea3d7d3aa9e8e615b1bc7c8bf232271f7
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-17 22:19:38 +0100

    File updated at 8/17/2025, 10:19:38 PM

--- 2796-RepeatString/2796-RepeatString.ts
+++ 2796-RepeatString/2796-RepeatString.ts
@@ -1,10 +1,8 @@
-// Last updated: 8/17/2025, 10:19:24 PM
+// Last updated: 8/17/2025, 10:19:38 PM
 interface String {
     replicate(times: number): string;
 }
 
 String.prototype.replicate = function (times) {
-  let repl = '';
-  for (let i = 0; i < times; i++) repl += this;
-  return repl;
+    return this.repeat(times)
 }
