af7875ea565d89a58f812edd336cebdcaa090318
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-20 23:17:17 +0100

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.ts
@@ -0,0 +1,25 @@
+// Last updated: 8/20/2025, 11:17:17 PM
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+
+    if (!list1) return list2;
+    if (!list2) return list1;
+
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
+    }
+};
