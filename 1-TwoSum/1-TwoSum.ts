3330a7612bc0a8d3318ce1f6cb7bcea00961bc60
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-23 21:44:26 +0100

    File updated at 8/23/2025, 9:44:26 PM

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,14 +1,12 @@
-// Last updated: 8/23/2025, 2:23:07 PM
+// Last updated: 8/23/2025, 9:44:26 PM
 function twoSum(nums: number[], target: number): number[] {
-    const map: { [key: number]: number } = {};
-
+    const hashmap: Record<number, number> = {};
     for (let i = 0; i < nums.length; i++) {
-        const complement = target - nums[i];
-        if (map[complement] !== undefined) {
-            return [map[complement], i];
+        const diff = target - nums[i];
+        if (hashmap.hasOwnProperty(diff)) {
+            return [hashmap[diff], i];
         }
-        map[nums[i]] = i;
+        hashmap[nums[i]] = i;
     }
-
-    throw new Error("No two sum solution");
+    return[];
 }
