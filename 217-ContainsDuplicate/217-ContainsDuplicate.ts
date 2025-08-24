214974f8528a251fdb80d73a559e770c16f3ddff
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-24 14:45:32 +0100

    File updated at 8/24/2025, 2:45:32 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.ts
+++ 217-ContainsDuplicate/217-ContainsDuplicate.ts
@@ -1,4 +1,4 @@
-// Last updated: 8/5/2025, 11:19:17 PM
+// Last updated: 8/24/2025, 2:45:32 PM
 function containsDuplicate(nums: number[]): boolean {
     const set = new Set<number>(nums);
     return (set.size < nums.length);
