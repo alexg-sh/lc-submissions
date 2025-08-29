36295b3d0a547b1fd58ec94c3e700fa170c0e03f
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-29 21:49:01 +0100

    File updated at 8/29/2025, 9:49:01 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.ts
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.ts
@@ -1,21 +1,34 @@
-// Last updated: 8/5/2025, 11:19:34 PM
+// Last updated: 8/29/2025, 9:49:01 PM
 function maxArea(height: number[]): number {
-    let left = 0;
-    let right = height.length - 1;
-    let maxArea = 0;
+  // we'll use the 2 pointer approach to solve the problem
+  // as we need to the maximum amount of water the container
+  // can store, we'll have the pointers from 0 till n-1
+  let low = 0;
+  let high = height.length - 1;
+  let ans = 0;
 
-    while (left < right) {
-        // Calculate area
-        const width = right - left;
-        const h = Math.min(height[left], height[right]);
-        maxArea = Math.max(maxArea, width * h);
+  while (low <= high) {
+    // we'll calcuate the length and breadth of the container
+    // and then we'll keep track of the highest value
 
-        // Move the pointer pointing at the smaller height
-        if (height[left] < height[right]) {
-            left++;
-        } else {
-            right--;
-        }
-    }
-    return maxArea;
-}
+    // as the note in the problem says that we cannot slant
+    // the container, so for length we need to take the lowest
+    // value else the line will not be parallel to the x-axis
+    // and we'll have a slant
+    const length = Math.min(height[low], height[high]);
+
+    // for breadth we need to find the distance between the 2 pointers
+    const breadth = high - low;
+    const area = length * breadth;
+
+    // store the highest value
+    ans = Math.max(ans, area);
+
+    // now we need to move either the i pointer or j pointer
+    // as we require the container to be max, we need to move
+    // the smallest value between low and high pointer
+    if (height[low] < height[high]) low++;
+    else high--;
+  }
+  return ans;
+};
