fa91a57f074b3632421ef96f40b1919bbb43e9b1
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-30 23:52:31 +0100

    Create 349-IntersectionofTwoArrays

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.ts
@@ -0,0 +1,13 @@
+// Last updated: 8/30/2025, 11:52:31 PM
+function intersection(nums1: number[], nums2: number[]): number[] {
+    const set1 = new Set(nums1);
+    const set2 = new Set(nums2);
+    const result: number[] = [];
+    
+    for (const num of set1) {
+        if (set2.has(num)) {
+            result.push(num);
+        }
+    }
+    return result;
+}
