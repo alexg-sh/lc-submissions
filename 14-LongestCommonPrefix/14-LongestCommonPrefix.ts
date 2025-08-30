bed6ec7032395bb85dd591c7f8c08c10cdf4e221
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-08-30 22:37:30 +0100

    File updated at 8/30/2025, 10:37:30 PM

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.ts
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.ts
@@ -1,14 +1,13 @@
-// Last updated: 8/5/2025, 11:19:33 PM
+// Last updated: 8/30/2025, 10:37:30 PM
 function longestCommonPrefix(strs: string[]): string {
+    if (!strs.length) return "";
     let prefix = strs[0];
-    for(let i = 1; i < strs.length; i++){
-        while(prefix.length > 0){
-            if(strs[i].indexOf(prefix) === 0){
-                break;
-            }
-            prefix = prefix.slice(0, -1);
+
+    for (let i = 1; i < strs.length; i++) {
+        while (strs[i].indexOf(prefix) !== 0) {
+            prefix = prefix.substring(0, prefix.length - 1);
+            if (!prefix) return "";
         }
     }
-    
     return prefix;
-};
+}
