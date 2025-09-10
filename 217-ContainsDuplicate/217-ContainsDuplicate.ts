426d7742d16a1e4ab3f3d3919c6db60eddaa81c5
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-09-10 14:55:27 +0100

    File updated at 9/10/2025, 2:55:27 PM

--- 217-ContainsDuplicate/217-ContainsDuplicate.ts
+++ 217-ContainsDuplicate/217-ContainsDuplicate.ts
@@ -1,12 +1,9 @@
-// Last updated: 9/8/2025, 3:33:29 PM
+// Last updated: 9/10/2025, 2:55:27 PM
 function containsDuplicate(nums: number[]): boolean {
-    nums.sort((a, b) => a - b);
+    let sort = nums.sort((a, b) => a - b);
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] == nums[i - 1]) {
-            return true
-        }
+        if (nums[i] == nums[i - 1]) return true
     }
     return false
-
 };
