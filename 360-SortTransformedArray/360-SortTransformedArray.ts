3d5cd5568c5dfdbb9d10aee30dfcde2d52c633d1
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-09-13 22:54:44 +0100

    Create 360-SortTransformedArray

---
+++ 360-SortTransformedArray/360-SortTransformedArray.ts
@@ -0,0 +1,10 @@
+// Last updated: 9/13/2025, 10:54:44 PM
+function sortTransformedArray(nums: number[], a: number, b: number, c: number): number[] {
+  let arr: number[] = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    arr.push(a * nums[i] * nums[i] + b * nums[i] + c);
+  }
+  arr.sort((x, y) => x - y);
+  return arr;
+}
