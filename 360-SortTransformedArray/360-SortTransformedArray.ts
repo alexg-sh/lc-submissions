042794c42a3a102247446d15ddfd31a52fcd6946
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-09-13 22:54:59 +0100

    File updated at 9/13/2025, 10:54:59 PM

--- 360-SortTransformedArray/360-SortTransformedArray.ts
+++ 360-SortTransformedArray/360-SortTransformedArray.ts
@@ -1,10 +1,40 @@
-// Last updated: 9/13/2025, 10:54:44 PM
+// Last updated: 9/13/2025, 10:54:59 PM
 function sortTransformedArray(nums: number[], a: number, b: number, c: number): number[] {
-  let arr: number[] = [];
+    const n = nums.length;
+    let res = new Array(n);
+    let left = 0, right = n - 1;
 
-  for (let i = 0; i < nums.length; i++) {
-    arr.push(a * nums[i] * nums[i] + b * nums[i] + c);
-  }
-  arr.sort((x, y) => x - y);
-  return arr;
+    // Helper to calculate f(x)
+    function quad(x: number): number {
+        return a * x * x + b * x + c;
+    }
+
+    // If a >= 0, fill from the end; otherwise, fill from the start
+    let index = a >= 0 ? n - 1 : 0;
+
+    while (left <= right) {
+        let leftVal = quad(nums[left]);
+        let rightVal = quad(nums[right]);
+
+        if (a >= 0) {
+            // Bigger values are on the outside
+            if (leftVal > rightVal) {
+                res[index--] = leftVal;
+                left++;
+            } else {
+                res[index--] = rightVal;
+                right--;
+            }
+        } else {
+            // Smaller values are on the outside
+            if (leftVal < rightVal) {
+                res[index++] = leftVal;
+                left++;
+            } else {
+                res[index++] = rightVal;
+                right--;
+            }
+        }
+    }
+    return res;
 }
