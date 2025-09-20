e0de134eade8aa11ae463fdb7fa60a51c65513c3
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-09-20 20:41:05 +0100

    Create 394-DecodeString

---
+++ 394-DecodeString/394-DecodeString.ts
@@ -0,0 +1,27 @@
+// Last updated: 9/20/2025, 8:41:05 PM
+function decodeString(s: string): string {
+    const stack: [string, number][] = [];
+    let currentString = "";
+    let currentNum = 0;
+
+    for (let char of s) {
+        if (!isNaN(Number(char))) {
+            // If the character is a digit, build the current number
+            currentNum = currentNum * 10 + Number(char);
+        } else if (char === '[') {
+            // Push the current string and number onto the stack
+            stack.push([currentString, currentNum]);
+            currentString = "";
+            currentNum = 0;
+        } else if (char === ']') {
+            // Pop from the stack and repeat the string
+            const [prevString, repeatNum] = stack.pop()!;
+            currentString = prevString + currentString.repeat(repeatNum);
+        } else {
+            // Build the current string with letters
+            currentString += char;
+        }
+    }
+
+    return currentString;
+}
