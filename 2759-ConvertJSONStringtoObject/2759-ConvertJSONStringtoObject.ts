3b6f07aa35a68fcd993a3722d48f2b13ff51147c
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-10-11 22:42:52 +0100

    File updated at 10/11/2025, 10:42:52 PM

--- 2759-ConvertJSONStringtoObject/2759-ConvertJSONStringtoObject.ts
+++ 2759-ConvertJSONStringtoObject/2759-ConvertJSONStringtoObject.ts
@@ -1,6 +1,6 @@
-// Last updated: 10/11/2025, 10:41:45 PM
+// Last updated: 10/11/2025, 10:42:52 PM
 type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
 
 function jsonParse(str: string): JSONValue {
-        return eval(`() => (${str})`)();
+        return JSON.parse(str);
 };
