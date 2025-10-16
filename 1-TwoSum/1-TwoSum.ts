69e37609b9a9a0d60a7d08ab16b31a7d3aad20f4
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-10-17 00:32:45 +0100

    File updated at 10/17/2025, 12:32:45 AM

--- 1-TwoSum/1-TwoSum.ts
+++ 1-TwoSum/1-TwoSum.ts
@@ -1,4 +1,4 @@
-// Last updated: 10/10/2025, 12:01:19 AM
+// Last updated: 10/17/2025, 12:32:45 AM
 function twoSum(nums: number[], target: number): number[] {
     const seen = new Map<number, number>();
     for (let i = 0; i < nums.length; i++) {
