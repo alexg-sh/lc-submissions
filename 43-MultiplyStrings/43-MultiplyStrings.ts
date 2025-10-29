f9d7cf2ad5c832dd5ecdde17de298533f759aab7
Alex Grigoras <113479287+alexg-sh@users.noreply.github.com>
2025-10-29 19:04:37 +0000

    File updated at 10/29/2025, 7:04:37 PM

--- 43-MultiplyStrings/43-MultiplyStrings.ts
+++ 43-MultiplyStrings/43-MultiplyStrings.ts
@@ -1,4 +1,4 @@
-// Last updated: 10/20/2025, 9:58:42 PM
+// Last updated: 10/29/2025, 7:04:37 PM
 function multiply(num1: string, num2: string): string {
     return (BigInt(num1) *BigInt(num2)).toString()
 };
